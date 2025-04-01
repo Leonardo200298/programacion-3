6ddbba83e23859049efc0a0155ba900f04b391b9
Leonardo200298 <leonardosanchez119@gmail.com>
2025-04-01 16:24:55 -0300

    parte del ejercicio 1 del tp nro 2 de arboles

---
+++ practico2punto1/Tree.java
@@ -0,0 +1,45 @@
+package practico2punto1;
+
+public class Tree {
+    	private TreeNode root;
+	
+	public Tree() {
+		this.root = null;
+	}
+	
+	public void add(Integer value) {
+		if (this.root == null)
+			this.root = new TreeNode(value);
+		else
+			this.add(this.root,value);
+	}
+	
+	private void add(TreeNode actual, Integer value) {
+		if (actual.getValue() > value) {
+			if (actual.getLeft() == null) { 
+				TreeNode temp = new TreeNode(value);
+				actual.setLeft(temp);
+			} else {
+				add(actual.getLeft(),value);
+			}
+		} else if (actual.getValue() < value) {
+			if (actual.getRight() == null) { 
+				TreeNode temp = new TreeNode(value);
+				actual.setRight(temp);
+			} else {
+				add(actual.getRight(),value);
+			}
+		}
+	}
+    public Integer getRoot(){
+        if (this.isEmpty()) {
+            
+            return -1;
+        }
+        return this.root.getValue();
+    }
+	public boolean isEmpty(){
+        return this.root.getValue() == null;
+    }
+}
+
