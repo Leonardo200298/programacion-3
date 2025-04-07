2b7a2447cc23c27293dc2009f80fd70cf5879176
Leonardo200298 <leonardosanchez119@gmail.com>
2025-04-07 13:40:03 -0300

    prueba de impresion preOrden en arboles

---
+++ practico2punto1/Main.java
@@ -0,0 +1,16 @@
+package practico2punto1;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree arbol = new Tree();
+        arbol.add(5);
+        arbol.add(2);
+        arbol.add(3);
+        arbol.add(4);
+        arbol.add(9);
+        arbol.add(8);
+
+        arbol.imprimirPreOrden(arbol.getRootNode());
+
+    }
+}
--- practico2punto1/Tree.java
+++ practico2punto1/Tree.java
@@ -1,45 +1,60 @@
 package practico2punto1;
 
 public class Tree {
-    	private TreeNode root;
-	
+
+	private TreeNode root;
+
 	public Tree() {
 		this.root = null;
 	}
-	
+
 	public void add(Integer value) {
 		if (this.root == null)
 			this.root = new TreeNode(value);
 		else
-			this.add(this.root,value);
+			this.add(this.root, value);
 	}
-	
+
 	private void add(TreeNode actual, Integer value) {
 		if (actual.getValue() > value) {
-			if (actual.getLeft() == null) { 
+			if (actual.getLeft() == null) {
 				TreeNode temp = new TreeNode(value);
 				actual.setLeft(temp);
 			} else {
-				add(actual.getLeft(),value);
+				add(actual.getLeft(), value);
 			}
 		} else if (actual.getValue() < value) {
-			if (actual.getRight() == null) { 
+			if (actual.getRight() == null) {
 				TreeNode temp = new TreeNode(value);
 				actual.setRight(temp);
 			} else {
-				add(actual.getRight(),value);
+				add(actual.getRight(), value);
 			}
 		}
 	}
-    public Integer getRoot(){
-        if (this.isEmpty()) {
-            
-            return -1;
-        }
-        return this.root.getValue();
-    }
-	public boolean isEmpty(){
-        return this.root.getValue() == null;
-    }
-}
 
+	public Integer getRoot() {
+		if (this.isEmpty()) {
+
+			return -1;
+		}
+		return this.root.getValue();
+	}
+
+	public TreeNode getRootNode() {
+		return this.root;
+	}
+	
+
+	public boolean isEmpty() {
+		return this.root.getValue() == null;
+	}
+
+	public void imprimirPreOrden(TreeNode nodo) {
+		if (nodo == null)
+			return;
+		System.out.print(nodo.getValue() + " ");
+		imprimirPreOrden(nodo.getLeft());
+		imprimirPreOrden(nodo.getRight());
+	}
+}
